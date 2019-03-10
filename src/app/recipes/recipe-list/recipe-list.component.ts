575117a63d708d04eb67b5cedb08cb4d54104d5b
Pawel Kaminski <kaminskipawel@hotmail.com>
2019-03-10 18:39:26 +0100

    Add Recipe model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Śledź w śmietanie', 'Metafizyczne danie',
+      'https://3.bp.blogspot.com/-7UM1Vnwks-s/VyZIylkkD5I/AAAAAAAAFOk/yJEA4T9DaBcgNIZIf0bb0RtnBHjTqSpxQCLcB/s1600/1DSC_2875.JPG'),
+    new Recipe('Kurczę pieczone', 'O kurczę',
+      'https://www.chowstatic.com/assets/recipe_photos/10530_roasted_rosemary_roasted_chicken.jpg')
+
+  ];
 
   constructor() { }
 
