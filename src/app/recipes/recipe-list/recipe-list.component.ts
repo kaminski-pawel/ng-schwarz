14bbd7d5c88aa2040b069fc6ecada150db6352a4
Pawel Kaminski <kaminskipawel@hotmail.com>
2019-03-21 21:30:07 +0100

    Add base for recipe-item component

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 
@@ -19,6 +19,10 @@ export class RecipeListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+  }
+
+  onSelected() {
+
   }
 
 }
